60c3ae170d6588e4711e17c50d86c7ed8f317b8e
Tim Paine <t.paine154@gmail.com>
2022-04-16 16:18:16 -0400

    update build script, fix lints

--- docs/conf.py
+++ docs/conf.py
@@ -163,9 +163,7 @@ latex_documents = [
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [
-    (master_doc, "fsspec_data", "fsspec_data Documentation", [author], 1)
-]
+man_pages = [(master_doc, "fsspec_data", "fsspec_data Documentation", [author], 1)]
 
 
 # -- Options for Texinfo output -------------------------------------------
